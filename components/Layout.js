f2988f56062dced0e05e514f6518f019ec714350
VictorAnizar <victoranizarm@live.com>
2022-03-09 15:02:15 -0600

    terminando formulario para login

---
+++ components/Layout.js
@@ -0,0 +1,48 @@
+import react from "react";
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+import { useRouter } from "next/router"
+const Layout = ({ children }) => {
+
+    const router = useRouter();
+
+    return (
+        <>
+            <Head>
+                <title>CRM - Admin</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <script src="https://cdn.tailwindcss.com"></script>
+            </Head>
+
+            {
+                router.pathname === "/login" || router.pathname === "/nuevaCuenta" ? (
+                    <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+                        <div>
+                            {children}
+                        </div>
+                    </div>
+                ) : (
+                    <div className="bg-gray-200 min-h-screen">
+                        <div className="flex min-h-screen">
+                            <Sidebar />
+
+                            <main className="sm:w-3/4 xl:4/5 sm:min-h-screen p-5">
+                                {children}
+
+                            </main>
+
+                        </div>
+
+
+                    </div >
+
+                )
+            }
+
+
+
+        </>
+    )
+}
+
+export default Layout;
---
+++ pages/nuevaCuenta.js
@@ -0,0 +1,16 @@
+import react from "react";
+import Layout from "../components/Layout";
+
+
+
+const NuevaCuenta = ()=>{
+    return (
+        <>
+            <Layout>
+                <h1>NuevaCuenta</h1>
+            </Layout>
+        </>
+    )
+}
+
+export default NuevaCuenta;
